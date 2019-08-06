69062eb0a46d2a7bfaeaf13a1cf099fe80241fb8
Patrick O'Shea <patrickodpt@gmail.com>
2019-08-06 13:46:15 -0400

    working on js pseudocode

---
+++ blackjack.js
@@ -0,0 +1,27 @@
+//function
+function startGame() {
+  //on button click load gameboard
+}
+
+//initialize global objects/arrays
+let pHand = []
+let dHand = []
+let deck = {
+  s: ,
+  h: ,
+  d: ,
+  c:
+}
+
+
+
+function deal() {
+  // called on click of deal button
+  // returns card
+}
+
+if (pHand.length < 2){
+  //give card from deal to player
+} elseif (dHand.length <2) {
+  //give card to dealer
+}
