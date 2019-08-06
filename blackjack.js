51275322360ce993effdb04b7890e7746ecb9fd3
Patrick O'Shea <patrickodpt@gmail.com>
2019-08-06 14:32:05 -0400

    in online research, realized there is no '1' in a deck of cards

--- blackjack.js
+++ blackjack.js
@@ -7,10 +7,10 @@ function startGame() {
 let pHand = []
 let dHand = []
 let deck = {
-  spade: ['1','2','3','4','5','6','7','8','9','10','J','Q','K','A'],
-  heart: ['1','2','3','4','5','6','7','8','9','10','J','Q','K','A'],
-  diamond: ['1','2','3','4','5','6','7','8','9','10','J','Q','K','A'],
-  club: ['1','2','3','4','5','6','7','8','9','10','J','Q','K','A']
+  spade: ['2','3','4','5','6','7','8','9','10','J','Q','K','A'],
+  heart: ['2','3','4','5','6','7','8','9','10','J','Q','K','A'],
+  diamond: ['2','3','4','5','6','7','8','9','10','J','Q','K','A'],
+  club: ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
 }
 
 
@@ -19,8 +19,8 @@ function deal() {
   // called on click of deal button
   // returns card from deck
 
-  suitRoll() => {Math.floor(Math.random() * 4)} //output will be between 0-3
-  cardRoll() => {Math.floor(Math.random() * 14)} //output will be between 0-13
+  suitRoll() => {return Math.floor(Math.random() * 4)}//output will be between 0-3
+  cardRoll() => {return Math.floor(Math.random() * 13)} //output will be between 0-12
   console.log("This is a suitRoll test: ", suitRoll())
   console.log("This is a suitRoll test: ", suitRoll())
   console.log("This is a suitRoll test: ", suitRoll())
@@ -41,32 +41,32 @@ function deal() {
   //   console.log("something in random rolls went wrong,")
   // }
 }
-
-if (pHand.length < 2){
-  // call deal() to give card to pHand.push
-} elseif (dHand.length <2) {
-  // call deal() to give card to dHand.push
-}
-
-
-function hit() {
-  //get card from deck
-  //append card to pHand
-}
-
-function stand() {
-  //run dealers play
-}
-
-function dealerPlays () {
-  // check value of dealer hand
-  // if value is >17 stay
-  // else hit
-}
-// after dealerPlays check who wins
-function winCheck(pHandValue, dHandValue) {
-  // compare value of dHand to pHand
-  // if dHand > pHand: dealer wins
-  // if dHand < pHand: player wins
-  // if dHand == pHand: timeout, no one wins / tie
-}
+//
+// if (pHand.length < 2){
+//   // call deal() to give card to pHand.push
+// } elseif (dHand.length <2) {
+//   // call deal() to give card to dHand.push
+// }
+//
+//
+// function hit() {
+//   //get card from deck
+//   //append card to pHand
+// }
+//
+// function stand() {
+//   //run dealers play
+// }
+//
+// function dealerPlays () {
+//   // check value of dealer hand
+//   // if value is >17 stay
+//   // else hit
+// }
+// // after dealerPlays check who wins
+// function winCheck(pHandValue, dHandValue) {
+//   // compare value of dHand to pHand
+//   // if dHand > pHand: dealer wins
+//   // if dHand < pHand: player wins
+//   // if dHand == pHand: timeout, no one wins / tie
+// }
